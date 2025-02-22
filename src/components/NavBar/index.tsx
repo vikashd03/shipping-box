68677f95ee7d853d23c065731c30c2526b933ecd
vikashd03 <vikashdhanabal003@gmail.com>
2025-02-22 19:06:26 +0530

    fixed nav height

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -11,7 +11,7 @@ const NavBar = () => {
   const handleNavItemClick = (navItem: any) => navigate(navItem.url);
 
   return (
-    <div className="flex h-14 items-center justify-between gap-4 overflow-x-auto border border-b bg-slate-100 px-3 py-2 sm:h-16 sm:px-5 sm:py-5">
+    <div className="flex h-14 items-center justify-between gap-4 overflow-x-auto border border-b bg-slate-100 px-3 py-2 sm:h-16 sm:px-5 sm:py-3">
       <div className="whitespace-nowrap text-2xl font-extrabold">
         Shipping Box
       </div>
